935c3c626d6b4f3fbf8d3cd1fcf3870871d89be7
Brent Reno <brent.reno@gmail.com>
2016-11-16 09:03:25 -0600

    basic server, serving index

---
+++ Server/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser");
+var urlencodedParser = bodyParser.urlencoded( { extended: true } );
+var bpJason = bodyParser.json();
+var port = process.env.PORT || 3030;
+var path = require('path');
+
+app.use( express.static( 'public' ) );
+app.use(bpJason);
+app.use(urlencodedParser);
+
+app.listen(port, function(){
+  console.log('server up on:', port);
+});
+
+app.get('/', function(req, res){
+  console.log('base url hit');
+  res.sendFile(path.resolve('server/public/views/index.html'));
+});
