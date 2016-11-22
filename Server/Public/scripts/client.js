364b6847e2b248482316a999f68b775df048946a
Brent Reno <brent.reno@gmail.com>
2016-11-22 13:44:57 -0600

    implement smooth scroll feature

--- Server/Public/scripts/client.js
+++ Server/Public/scripts/client.js
@@ -2,4 +2,13 @@ console.log("client.js working");
 
 $(document).ready(function(){
   console.log("JS is Ready");
+  // smooth-scroll
+  $('.smooth').on('click', function() {
+    $.smoothScroll({
+        scrollElement: $('body'),
+        scrollTarget: '#' + this.id
+    });
+
+    return false;
+});
 });
