dad05b4cbab3ee1006f7fe96aea5397688466cb3
Brent Reno <brent.reno@gmail.com>
2017-01-19 17:37:05 -0600

    smooth out sticky nav bar movement

--- Server/Public/scripts/client.js
+++ Server/Public/scripts/client.js
@@ -4,7 +4,6 @@ $(document).ready(function(){
   var nav = $(".nav");
   $(window).scroll(function(){
     if( $(this).scrollTop() > 650){
-      console.log(650);
       nav.addClass(" nav-scrolled");
     } else{
       nav.removeClass(" nav-scrolled");
