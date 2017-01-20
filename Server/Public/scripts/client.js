efbeb177fc31969cf9336ed5f5554d1fca1171a7
Brent Reno <brent.reno@gmail.com>
2017-01-20 15:05:52 -0600

    smooth scroll working

--- Server/Public/scripts/client.js
+++ Server/Public/scripts/client.js
@@ -2,11 +2,21 @@
 $(document).ready(function(){
   console.log("ready");
   var nav = $(".nav");
+  //nav bar will stick to top of window on scroll
   $(window).scroll(function(){
     if( $(this).scrollTop() > 650){
       nav.addClass(" nav-scrolled");
     } else{
       nav.removeClass(" nav-scrolled");
     }
+  });
+  //smooth scroll to section on nav click
+  $(".scroll").click(function(event){
+		event.preventDefault();
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+	});
+  //smooth scroll to top of page aka home
+  $(".scroll-home").click(function(event){
+    $('html,body').animate({scrollTop:$('body').offset().top}, 500);
   });
 });
